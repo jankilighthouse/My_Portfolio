1ee7487af0e2a04bfa5e0f32b7735c7bf083c78a
jankilighthouse <jankipatel6890@gmail.com>
2019-04-07 19:35:46 +0000

    saved all changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,6 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-
 const navItems = document.querySelectorAll('.nav-item');
 
 //Set initial state of menu
